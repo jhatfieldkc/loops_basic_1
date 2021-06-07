089a1907373cdf04d28833e7b59aea4cbdb497a7
Jeremy Hatfield <jhatfieldkc@gmail.com>
2021-06-07 17:51:22 -0500

    final commit

---
+++ loops_basic_1.py
@@ -0,0 +1,43 @@
+# 1. Basics
+
+# for x in range(151):
+#     print(x)
+
+# 2. Multiples of 5
+
+# for x in range(5, 1005, 5):
+#     print(x)
+
+# 3.  Dojo way of counting
+
+# for x in range(101):
+#     if x % 10 == 0:
+#         print("Coding Dojo")
+#     elif x % 5 == 0:
+#         print("Coding")
+#     else:
+#         print(x)
+
+# 4.  Whoa that sucker's huge
+# total = 0
+# for x in range(0,500000):
+#     if x % 2 != 0:
+#         total += x
+#     elif x % 2 == 0:
+#         continue
+#     print(total)
+
+# 5. Countdown by fours
+
+# for x in range(2018, 0, -4):
+#     print(x)
+
+# 6.  Flexible Counter
+
+# lowNum = 2
+# highNum = 9
+# mult = 3
+
+# for x in range(lowNum, highNum+1):
+#     if x % mult == 0:
+#         print(x)
